6b6cab2c6844e924faa7fef39e006878871d495c
csyhuang <csyhuang@uchicago.edu>
2022-08-15 20:27:32 -0500

    update instructions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # google_scholar_scraper
-Scrape google scholar search results
+
+Minimal requirement to run the scripts:
+- Get an account on serpapi.com
+- Install the serpapi python package
+- Put your Serp API key in the file `serp_api_key`
+
+Main script to run: `main.py`
+- The dictionary `list_of_articles_to_check` contains the `ID -> title` map of my articles
+- To find your article ID, click onto the number of citations of your article on Google Scholar. You can then find the ID from the URL.  
--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import citation_utilities
 
 # Input parameters by user
 list_of_articles_to_check = {
-    3225371177020252908: 100,   # Atmospheric blocking as a traffic jam in the jet stream
-    10477703589246425643: 78,   # Local finite-amplitude wave activity as a diagnostic of anomalous weather events
-    12098694703830025611: 22,   # Local wave activity budgets of the wintertime Northern Hemisphere: Implication...
-    14468658417246256587: 19,   # Role of finite-amplitude Rossby waves and nonconservative processes in downward...
-    17382647637759034075: 13,   # Local wave activity and the onset of blocking along a potential vorticity front
-    15968917917766089864: 11,   # Role of finite-amplitude eddies and mixing in the life cycle of SSW
-    5485128022826263690: 3}     # The 2021 Pacific Northwest heat wave and associated blocking...
+    3225371177020252908: "Atmospheric blocking as a traffic jam in the jet stream",
+    10477703589246425643: "Local finite-amplitude wave activity as a diagnostic of anomalous weather events",
+    12098694703830025611: "Local wave activity budgets of the wintertime Northern Hemisphere: Implication for the " +
+                          "Pacific and Atlantic storm tracks",
+    14468658417246256587: "Role of finite-amplitude Rossby waves and nonconservative processes in downward migration " +
+                          "of extratropical flow anomalies",
+    17382647637759034075: "Local wave activity and the onset of blocking along a potential vorticity front",
+    15968917917766089864: "Role of finite-amplitude eddies and mixing in the life cycle of stratospheric sudden " +
+                          "warmings",
+    5485128022826263690: "The 2021 Pacific Northwest heat wave and associated blocking: Meteorology and the role of " +
+                         "an upstream cyclone as a diabatic source of wave activity"}
 citation_records_dir: str = 'citation_records'  # Name of directory where the retrieved citations will be exported to
 json_path = None  # It can be some existing JSON file retrieved
 
